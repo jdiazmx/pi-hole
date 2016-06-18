c441c9d727107eb9eb884b42de59f63db687e5ef
Mcat12 <newtoncat12@yahoo.com>
2016-06-18 15:10:00 -0400

    Check modification dates
    Uses Requests library (will need to be installed). This library
    is so much more concise and can easily use different HTTP verbs,
    such as HEAD, to minimize the strain on list bandwidth.

--- scripts/gravity.new.py
+++ scripts/gravity.new.py
@@ -13,28 +13,52 @@
 
 # IMPORTS
 
-
 import pihole_vars
 from urllib.parse import urlparse
-from urllib.request import urlopen
-
+import requests
+from datetime import datetime
+import email.utils as eut
 
 # SCRIPT
 
+pihole = pihole_vars.Pihole()
 
-pihole = Pihole()
+
+# Downloads a list
+def download_list(list):
+    print(" Downloaded!")
+
 
 # Check for updates
 for l in pihole.lists:
+    # Get domain for output
     domain = '{uri.netloc}'.format(uri=urlparse(l.uri))
-    print("Initializing pattern buffer for " + domain + "...")
+    print("Initializing pattern buffer for " + domain + "...", end="")
 
     # Check if the list has been downloaded
     domains = l.get_domains()
     if len(domains) == 0:
         # Must be a new list
-        pass # Placeholder
+        print(" New list, downloading...", end="")
+        download_list(l)
     # Check if it needs updating
     else:
-        # Check date-modified
-        remote_m = urlopen(l.uri, timeout=5).headers["Last-Modified"]
+        # Get request
+        remote = requests.head(l.uri, timeout=5)
+
+        # Check for Last-Modified header
+        if "Last-Modified" in remote.headers and len(remote.headers["Last-Modified"]) > 0:
+            remote_date = datetime(*eut.parsedate(remote.headers["Last-Modified"])[:6])
+
+            # If the remote date is newer than the stored date
+            if remote_date > l.get_date():
+                print(" Update found, downloading...", end="")
+                download_list(l)
+            else:
+                print(" No update!")
+                pass
+        else:
+            # If we don't know the date, just download it
+            print(" No modification date found, downloading...", end="")
+            download_list(l)
+
