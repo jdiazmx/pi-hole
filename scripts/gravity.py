628a550f71342d7be837d756e8de7eb34af92cc5
Mcat12 <newtoncat12@yahoo.com>
2016-07-04 11:52:52 -0400

    Replace old gravity with new gravity

--- scripts/gravity.py
+++ scripts/gravity.py
@@ -15,182 +15,94 @@
 
 
 import pihole_vars
-from urllib.request import urlopen
-import os
-import time
-import datetime
-import re
 from urllib.parse import urlparse
+import requests
+from datetime import datetime
+import email.utils as eut
 
 
-# FUNCTIONS
+# SCRIPT
 
 
-def local_calibration():
-    if os.path.isfile(pihole_vars.pihole_conf):
-        print("Local calibration requested.  Scanning...")
-    if os.path.isfile(pihole_vars.custom_ad_list):
-        print("Custom ad lists detected.  Scanning...")
+# Downloads a list
+def download_list(list, mod, pihole):
+    # Clean old list
+    list.clean()
+
+    # Get new list
+    r = requests.get(list.get_uri(), timeout=5)
+
+    # Parse domains into list (removes comments)
+    domains = [domain.split(" ")[1] for domain in r.text.splitlines() if
+               not domain.strip().startswith("#") and len(domain.strip()) > 0]
+
+    pihole.update_list(list.get_uri(), domains, mod)
+
+    print("  * Downloaded!")
+
+    return len(domains)
 
 
-# Be a respectful netizen by only downloading the list if it is newer than the local file
-def transport_buffer(url, filename):
-    # Get the list
-    remote_file = urlopen(url, timeout=5)
-    try:
-        # Get the header from the list so the modification time can be checked
-        last_modified = remote_file.headers["Last-Modified"]
+def main():
+    print("Loading Pi-hole instance...")
+    num_pre_formatted = 0
+    pihole = pihole_vars.Pihole()
 
-        # adblock.gjtech.net returns zero, so this prevents it from diplaying that the pattern check was successful and inconclusive
-        if last_modified == "0":
-            pass
-        else:
-            print ("  * Pattern check successful.")
-
-        # Convert time to epoch for easy comparison
-        temp_time = time.strptime(last_modified, "%a, %d %b %Y %H:%M:%S %Z")
-        remote_mtime = time.mktime(temp_time)
-
-        if os.path.isfile(filename):
-            # Get the modification time of the local file so it can be comapred with the remote one
-            local_mtime = os.path.getmtime(filename)
-            print("  * Existing list detected.   (" + str(local_mtime) + ")")
-
-            # Compare the local mtime to the remote mtime.  <------THIS IS BROKEN STILL, but the functionality is there
-            # If they are the same, set to False so the list will not be downloaded in grvity_well
-            # Since the content is the same, there is no need to re-download it
-            if local_mtime > remote_mtime:
-                print("  * List is up-to-date.")
-                return False
-            # Otherwise, the list on the server is newer and should be downloaded (set to True)
-            else:
-                print("  * Remote list is newer.     (" + str(remote_mtime) + ")")
-                return True
-        # If there isn't an existing file, it's a new list (or the user deleted the file)
-        else:
-            print("  * New list detected.")
-            return False
-    # Return true to download the list since the mod time was not available
-    # This will get the latest file because we don't know if the existing one is out-of-date or not
-    except:
-        print("  * Pattern check was inconclusive.")
-        return True
-
-
-# Downloads the blacklists
-def gravity_well():
-    # For each URL in the sources list
-    for idx, url in enumerate(pihole_vars.sources):
-        # Get just the domain name so it can be displayed to the user during processing
-        parsed_uri = urlparse(url)
-        domain = '{uri.netloc}'.format(uri=parsed_uri)
-        # Set the filename for the downloaded list
-        # Save the file as list.n.domain.name.domains
-        # This is useful for debugging as well as keeping the lists out of RAM
-        filename = "list." + str(idx) + "." + domain
-
+    # Check for updates
+    for l in pihole.lists:
+        # Get domain for output
+        domain = '{uri.netloc}'.format(uri=urlparse(l.get_uri()))
         print("Initializing pattern buffer for " + domain + "...")
 
-        # If there is already an existing file
-        if os.path.isfile(filename):
-            # Be a respectful netizen by only downloading the list if it is newer by running it though the trasnport_buffer()
-            pattern_check = transport_buffer(url, filename)
-            # If there was not a timestamp from the server, just let the user know and download the list anyway
-            # If it does have a timestamp and thus is older than what is found online, download the list
-            if pattern_check or pattern_check is None:
-                print("  * Downloading...")
-                print("")
-            # Otherwise, do not download so we can respect the list maintainer's bandwidth
-            elif not pattern_check:
-                print("  * Skipping...")
-                print("")
+        # Check if the list has been downloaded
+        if len(l.get_domains()) == 0:
+            # Must be a new list
+            print("  * New list, downloading...")
+            num_pre_formatted += download_list(l, datetime.now(), pihole)
+        # Check if it needs updating
+        else:
+            # Get request
+            remote = requests.head(l.get_uri(), timeout=5)
 
+            # Check for Last-Modified header
+            if "Last-Modified" in remote.headers and \
+                    len(remote.headers["Last-Modified"]) > 0 and \
+                    remote.headers["Last-Modified"] != '0':
+                remote_date = datetime(*eut.parsedate(remote.headers["Last-Modified"])[:6])
 
-# Finds and aggregates all of the ad list files
-def collapse():
-    print("Aggregating list of domains...")
-    with open(pihole_vars.matter, "w") as outfile:
-        for f in os.listdir(pihole_vars.pihole_dir):
-            if f.startswith(pihole_vars.list_prefix):
-                with open(f, "r") as infile:
-                    outfile.write(infile.read())
-
-
-# Line count to display quantity of domains to the user
-def particle_density(filename):
-    with open(filename) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-
-
-def matter_and_light():
-    print("Getting just the domain names...")
-    # Open the aggregated list
-    f = open(pihole_vars.matter, "r")
-    # Make a new file to put just the domain names in
-    with open(pihole_vars.and_light, "w") as and_light:
-        # For each line from the aggregated list,
-        for line in f.readlines():
-            # Find all the domains (P.S. I have no idea how the regex works, but it seems to)
-            line = re.findall(r'(\S+)', line)
-            # If there is not an entry, skip it
-            if not line:
-                pass
-            # Also pass if there is a comment
-            elif "#" in line[0]:
-                pass
-            # Also print the second field if the line starts with 127.0.0.1
-            elif line[0] == "127.0.0.1":
-                and_light.write("%s\n" % line[1])
-            # After all that, write the first field, which should be the domain name
+                # If the remote date is newer than the stored date
+                if remote_date > l.get_date():
+                    print("  * Update found, downloading...")
+                    num_pre_formatted += download_list(l, remote_date, pihole)
+                else:
+                    print("  * No update!")
+                    num_pre_formatted += len(l.get_domains())
+                    pass
             else:
-                and_light.write("%s\n" % line[0])
-    print(str(particle_density(pihole_vars.matter)) + " domains exist before refinement.")
+                # If we don't know the date, just download it
+                print("  * No modification date found, downloading...")
+                num_pre_formatted += download_list(l, datetime.now(), pihole)
+
+    # Condense into a formatted list of domains
+    print("Formatting " + str(num_pre_formatted) + " domains and removing duplicates...")
+    pihole.compile_list()
+
+    # Export domains to hosts file
+    print("Exporting " + str(len(pihole.get_domains())) + " domains...")
+    pihole.export_hosts()
+
+    # Whitelist adlist uris
+    print("Whitelisting x adlist sources...")
+
+    # Whitelist and Blacklist domains
+    print("Running whitelist script...")
+    print("  * Whitelisted x domains!")
+    print("Running blacklist script...")
+    print("  * Blacklisted x domains!")
+
+    # Reload dnsmasq to apply changes
+    print("Reloading dnsmasq...")
 
 
-def event_horizon():
-    print("Sorting and removing duplicates...")
-    # Open the list of just domain names
-    f = open(pihole_vars.and_light, "r")
-    # Keep track of the lines that already exist
-    lines_seen = set()
-    # Open the file the sorted list will go into
-    outfile = open(pihole_vars.event_horizon, "w")
-    # For each line in the domain list,
-    for line in open(pihole_vars.and_light, "r"):
-        # If the line is not a duplicate, write it to the new file
-        if line not in lines_seen:
-            lines_seen.add(line)
-    # Sort the list for readability
-    outfile.writelines(sorted(lines_seen))
-    print(str(particle_density(pihole_vars.event_horizon)) + " domains trapped in the event horizon.")
-
-
-def assimilate():
-    # Your technological distinctiveness will be added to our own
-    # Your domains will adapt to service us
-    # Resistance is futile
-    print("Formating domain list to HOSTS format...")
-    # Open the list of just domain names
-    f = open(pihole_vars.event_horizon, "r")
-    # Open the file for the formatted list
-    with open(pihole_vars.ad_list, "w") as ad_list:
-        for line in open(pihole_vars.event_horizon, "r"):
-            # Prepend each line with the Pi-hole's IP address and a space
-            ad_list.write(pihole_vars.pihole_ip + " " + line)
-
-# Download the blacklists
-gravity_well()
-
-# Find all of the downloaded lists
-collapse()
-
-# Extract just the domain names
-matter_and_light()
-
-# Sort and remove duplicates
-event_horizon()
-
-# Format the domains into host format
-assimilate()
+if __name__ == "__main__":
+    main()
