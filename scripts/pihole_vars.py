7b112b9e0fad6f483eba7b0d3c63f1682b4da31c
Mcat12 <newtoncat12@yahoo.com>
2016-07-16 19:48:02 -0400

    Set license to AGPL

--- scripts/pihole_vars.py
+++ scripts/pihole_vars.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
-# Pi-hole: A black hole for Internet advertisements
-# (c) 2015, 2016 by Jacob Salmela
-# Network-wide ad blocking via your Raspberry Pi
-# http://pi-hole.net
-# Pi-hole varibles and classes
+# Copyright (c) 2016 Jacob Salmela
+# Pi-hole: a DNS based ad-blocker [https://www.pi-hole.net]
 #
-# Pi-hole is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+# Pi-hole variables and classes
+#
+# The Pi-Hole is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 # IMPORTS
