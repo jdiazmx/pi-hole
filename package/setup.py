6908692d7b1768746586965d6f261cdbad357168
Mcat12 <newtoncat12@yahoo.com>
2016-07-30 15:29:04 -0400

    Move scripts to bin
    `pihole` script will be exposed as a comamnd line script for users
    to use.

--- package/setup.py
+++ package/setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="Pi-hole",
     version="3.0.0",
-    packages=find_packages()
+    packages=find_packages(),
+    scripts=["bin/pihole"]
 )
