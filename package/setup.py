7ca24c906bf9bee49618191a872d68b1187bd521
Mcat12 <newtoncat12@yahoo.com>
2016-07-30 15:57:59 -0400

    Add dependencies

--- package/setup.py
+++ package/setup.py
@@ -5,6 +5,9 @@ setup(
     version="3.0.0",
     packages=find_packages(),
     scripts=["bin/pihole"],
+
+    install_requires=["docopt", "requests"],
+
     url="https://pi-hole.net",
     license="AGPL"
 )
